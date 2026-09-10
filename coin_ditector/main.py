5b6ba3351d955399edaa161d11c74da17ff17fe5
Amir <amirmahdi.haghverdi.8484@gmail.com>
2026-09-10 22:21:15 +0330

    commit

--- coin_ditector/main.py
+++ coin_ditector/main.py
@@ -1,15 +1,15 @@
 import cv2
 
-face_detector = cv2.CascadeClassifier("cascade/cascade.xml")
+coin_detector = cv2.CascadeClassifier("cascade/cascade.xml")
 
 cap = cv2.VideoCapture(0)
 
 while True:
     _,frame = cap.read()
 
-    faces = face_detector.detectMultiScale(frame)
+    coin = coin_detector.detectMultiScale(frame)
 
-    for (x,y,w,h) in faces:
+    for (x,y,w,h) in coin:
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
     cv2.imshow("frame",frame)
--- coin_ditector/test.py
+++ coin_ditector/test.py
@@ -1,3 +1,2 @@
-import os
-print(os.getcwd())  # پوشه‌ای که اسکریپت داره ازش اجرا میشه
-print(os.path.exists("cascade.xml"))  # باید True باشه
+import cv2
+print(cv2.__version__)
